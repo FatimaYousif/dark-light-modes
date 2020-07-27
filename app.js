77561c5e4848cb4ff9d7e540a2530e5ddc60b43e
Fatima Yousif <49322171+FatimaYousif@users.noreply.github.com>
2020-07-27 13:21:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,97 @@
+// function on()
+// {
+// document.querySelector("#on").style.display="none";
+// document.querySelector("#off").style.display="block";
+// document.querySelector(".row").style.display="flex";
+// document.querySelector(".row").style.justifyContent="center";
+
+// document.querySelector(".blue").style.color="yellow";
+// document.querySelector(".blue1").style.color="yellow";
+// document.querySelector(".blue2").style.color="yellow";
+// document.querySelector(".blue3").style.color="yellow";
+
+// // document.body.style.backgroundColor="black";
+// // document.body.style.color="white";
+
+// document.body.style.backgroundColor="black";
+// document.body.style.color="white";
+
+
+// document.querySelector("#purple").style.color="yellow";
+// document.querySelector("#what").style.color="red";
+
+
+// ddisplay flex and left align and justifycontent
+// document.querySelector("#purple").style.display="flex";
+// document.querySelector("#purple").style.justifyContent="spaceBetween";
+// document.querySelector("#purple").style.textAlign="left";
+
+// document.querySelector("#what").style.display="flex";
+// document.querySelector("#what").style.justifyContent="spaceBetween";
+// document.querySelector("#what").style.textAlign="left";
+
+// }
+
+
+function on()
+{
+document.querySelector("#on").style.display="none";
+document.querySelector("#off").style.display="block";
+document.querySelector(".row").style.display="flex";
+document.querySelector(".row").style.justifyContent="center";
+
+
+
+document.body.style.backgroundColor="black";
+document.body.style.color="white";
+
+document.querySelector(".blue").style.color="yellow";
+document.querySelector(".blue1").style.color="yellow";
+document.querySelector(".blue2").style.color="yellow";
+document.querySelector(".blue3").style.color="yellow";
+
+
+
+document.querySelector(".row ").style.display="flex";
+document.querySelector(".row ").style.justifyContent="space-between";
+document.querySelector(".row ").style.textAlign="left";
+
+}
+
+function off()
+{
+ document.querySelector("#on").style.display="block";
+document.querySelector("#off").style.display="none";
+
+document.querySelector(".row").style.display="flex";
+document.querySelector(".row").style.justifyContent="space-between";
+document.querySelector(".row").style.textAlign="left";
+
+
+document.body.style.backgroundColor="white";
+document.body.style.color="black";
+
+document.querySelector(".blue").style.color="blue";
+document.querySelector(".blue1").style.color="blue";
+document.querySelector(".blue2").style.color="blue";
+document.querySelector(".blue3").style.color="blue";
+
+
+document.querySelector("#purple").style.color="yellow";
+document.querySelector("#what").style.color="red";
+
+
+// ddisplay flex and left align and justifycontent
+// document.querySelector("#purple").style.display="flex";
+// document.querySelector("#purple").style.justifyContent="spaceBetween";
+// document.querySelector("#purple").style.textAlign="left";
+
+// document.querySelector("#what").style.display="flex";
+// document.querySelector("#what").style.justifyContent="spaceBetween";
+// document.querySelector("#what").style.textAlign="left";
+
+
+}
+
+
+
